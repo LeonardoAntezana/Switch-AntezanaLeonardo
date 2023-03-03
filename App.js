ebca062b0decf6c30f5632eeed66d2b692db3b5d
¨Leonardo <leonardoantezana59@gmail.com>
2023-03-03 12:19:02 -0300

    agregando logica y estilos a GameScreen, cargando fonts y haciendo switch de pantallas

--- App.js
+++ App.js
@@ -1,16 +1,36 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import { StyleSheet, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import { Header } from './src/components';
 import  StartGameScreen  from './src/screens/StartGameScreen';
 import GameScreen from './src/screens/GameScreen';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if(fontsLoaded){
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
   const [userNumber, setUserNumber] = useState()
 
   const startGameHandler = numberSelected => setUserNumber(numberSelected)
 
+  if(!fontsLoaded){
+    return null
+  }
+
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <Header title='Adivina el numero'/>
       {userNumber ? <GameScreen/> : <StartGameScreen startGameHandler={startGameHandler}/>}
     </View>
--- src/screens/GameScreen.jsx
+++ src/screens/GameScreen.jsx
@@ -1,14 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { React, useState } from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import { Card, NumberContainer } from '../components'
 
-const GameScreen = () => {
+const GameScreen = ({userOption}) => {
+
+  const generateRandomBetween = (min, max, exclude) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+    if (rndNum === exclude) {
+        return generateRandomBetween(min, max, exclude);
+    } else {
+        return rndNum;
+    }
+  }
+  
+  const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, userOption))
+
   return (
-    <View>
-      <Text>GameScreen</Text>
+    <View style={styles.screen}>
+      <Text style={styles.text}>La suposicion del oponente</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title='menor' onPress={() => {}}/>
+        <Button title='mayor' onPress={() => {}}/>
+      </Card>
     </View>
   )
 }
 
 export default GameScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center',
+  },
+  text: {
+    marginVertical: 10, 
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingVertical: 15,
+    marginTop: 10,
+    width: 250,
+    maxWidth: '80%',
+  }
+})
